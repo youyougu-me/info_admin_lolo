204a9eacd7cdee0b67b0a957b2bee90835be0572
youyougu-me <945029149@qq.com>
2020-07-08 17:02:16 +0800

    first commit

---
+++ src/components/Pagination/pagination.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <!-- 记住当前页和页码容量自己会变,写死即可 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[5,8,10]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="ownTotal"
+      background
+    ></el-pagination>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      //这个由父亲决定
+      ownTotal: 0
+    };
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.$emit("pageSizeChange", val);
+    },
+    handleCurrentChange(val) {
+      this.$emit("currentPageChange", val);
+    }
+  },
+  created() {},
+  props: {
+    total: {
+      default: () => {
+        return undefined;
+      }
+    },
+    currentPage: {
+      default: () => {
+        return undefined;
+      }
+    },
+    pageSize: {
+      default: () => {
+        return undefined;
+      }
+    }
+  },
+  mounted() {},
+  watch: {
+    total(newVal, oldVal) {
+      this.ownTotal = newVal;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+//我只负责改变父组件的两个页码参数
+//父组件给我传总页数
+</style>
